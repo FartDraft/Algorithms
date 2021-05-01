de6c85fcc1307af3223df6ae625ff1fa273d0bbd
FartDraft <fartdraft@gmail.com>
2021-05-01 21:35:47 +0300

    Добавлена функция Vinograd_Strassen. "О большое" обозначения заменены на "Тета" обозначения там, где надо. Функция is_simple исправлена.

--- main.py
+++ main.py
@@ -3,9 +3,6 @@ __author__ = "Afanasin Egor"
 __email__ = "fartdraft@gmail.com"
 
 
-from math import trunc, sqrt
-
-
 def is_simple(num: int) -> bool:
     """Проверка числа num на простоту.
 
@@ -15,7 +12,11 @@ def is_simple(num: int) -> bool:
     Returns:
         bool: True - если число num простое, иначе False.
     """
-    if num < 2 or not num % 2 or not num % 3:
+    from math import trunc, sqrt
+
+    if num == 2 or num == 3:
+        return True
+    elif num < 2 or not num % 2 or not num % 3:
         return False
     for k in range(6, trunc(sqrt(num)) + 2, 6):
         if not num % (k - 1) or not num % (k + 1):
@@ -34,6 +35,8 @@ def sieve(n: int) -> list:
         list: массив bool значений. 
             Каждый индекс соответствует числу. True - если число простое, иначе - False.
     """
+    from math import trunc, sqrt
+
     assert n > 2, "n меньше чем 2"
     ret = [False, False] + [True] * (n - 1)
     for k in (2, 3):
@@ -48,7 +51,7 @@ def sieve(n: int) -> list:
 
 
 def insertion_sort(arr: list) -> None:
-    """Сортировка массива по возрастанию методом вставок. O(N**2).
+    """Сортировка массива по возрастанию методом вставок. Θ(N**2).
 
     Идея:
         В начале каждой итерации цикла for есть отсортированный подмассив из элементов arr[:top], но в отсортированном порядке.
@@ -68,7 +71,7 @@ def insertion_sort(arr: list) -> None:
 
 
 def selection_sort(arr: list) -> None:
-    """Сортировка массива по возрастанию методом выбора. O(N**2).
+    """Сортировка массива по возрастанию методом выбора. Θ(N**2).
 
     Идея:
         В каждой итерации цикла for ищет минимальный элемент из подмассива arr[pos:] и меняет его местами с arr[pos].
@@ -87,7 +90,7 @@ def selection_sort(arr: list) -> None:
     
 
 def bubble_sort(arr: list) -> None:
-    """Сортировка массива по возрастанию методом пузырька. O(N**2).
+    """Сортировка массива по возрастанию методом пузырька. Θ(N**2).
 
     Идея:
         В конце каждой итерации цикла (1) максимум из подмассива arr[:i + 1] стоит в arr[i].
@@ -111,7 +114,7 @@ def bubble_sort(arr: list) -> None:
 
 
 def merge_sort(arr: list) -> None:
-    """Сортировка слиянием. O(N log N).
+    """Сортировка слиянием. Θ(N log N).
 
     Идея:
         Парадигма "разделяй и властвуй".
@@ -132,7 +135,7 @@ def merge_sort(arr: list) -> None:
     """
     def _merge(arr: list, p: int, q: int, r: int) -> None:
         """Сливает два подмассива arr[p:q+1] и arr[q+1:r+1] в один отсортированный, элементы которого заменяют текущие
-        элементы подмассива arr[p:r+1]. O(N).
+        элементы подмассива arr[p:r+1]. Θ(N).
 
         Хитрость:
             В самый конец обоих подмассивов помещается бесконечно большое число. Благодаря этому в ходе каждой итерации
@@ -184,7 +187,7 @@ def merge_sort(arr: list) -> None:
 
 def bin_search(seq, x):
     """Алгоритм бинарного поиска элемента x в последовательности seq. Последовательность должна быть 
-    отсортирована по возрастанию! O(log N).
+    отсортирована по возрастанию! Θ(log N).
 
     Идея:
         Находим средний элемент последовательности (seq[m]). Если он меньше искомого, то сдвигаем начало левой границы на 
@@ -212,11 +215,11 @@ def bin_search(seq, x):
 
 
 def inversions_num(arr: list) -> int:
-    """Алгоритм для нахождения количества инверсий в массиве arr. O(N log N).
+    """Алгоритм для нахождения количества инверсий в массиве arr. Θ(N log N).
     
     Идея:
         Если i < j и arr[i] > arr[j], то пара (i, j) называется инверсией массива arr.
-        Для достижения времени O(N log N) модифицируем сортировку слиянием. В функции 
+        Для достижения времени Θ(N log N) модифицируем сортировку слиянием. В функции 
         слияния (m_merge) есть такой шаг, когда текущий элемент левой части больше 
         текущего элемента правой части. Вот в этот момент нужно к (локальному) счётчику
         инверсий (invcount) добавить количество ещё неиспользованных элементов левой 
@@ -258,7 +261,7 @@ def inversions_num(arr: list) -> int:
 def find_maximum_subarray(arr: list) -> tuple:
     """Алгоритм для нахождения непустого непрерывного подмассива массива arr, значения которого имеют наибольшую сумму. 
     Такой подмассив я называю максимальным. Возвращает (-1, -1, 0), если в исходном массиве arr нет ни одного 
-    положительного числа. O(N).
+    положительного числа. Θ(N).
 
      Идея:
         Максимальный подмассив массива arr[:i+1] представляет собой либо максимальный подмассив массива arr[:i], либо
@@ -289,6 +292,125 @@ def find_maximum_subarray(arr: list) -> tuple:
             sm = 0
             low = i + 1
     return max_low, max_high, max_sum
+
+
+def Vinograd_Strassen(a: list, b: list) -> list:
+    """Алгоритм Винограда-Штрассена для умножения квадратных матриц. Θ(N**log2(7)), O(N**2,81).
+    Матрицы a и b могут быть перемножены, если они совместимы в том смысле, что число столбцов матрицы a равно числу
+    строк матрицы b. Данная реализация алгоритма НЕ РАБОТАЕТ для прямоугольных матриц a и b вида m*n и n*p соответственно.
+    Это было сделано в целях ускорения времени выполнения.
+
+    Идея:
+        A11 A12     B11 B12     C11 C12
+        A21 A22  *  B21 B22  =  C21 C22
+        
+        S1 = A21 + A22
+        S2 = S1 - A11
+        S3 = A11 - A21
+        S4 = A12 - S2
+        S5 = B12 - B11
+        S6 = B22 - S5
+        S7 = B22 - B12
+        S8 = S6 - B21
+        
+        P1 = S2 * S6
+        P2 = A11 * B11
+        P3 = A12 * B21
+        P4 = S3 * S7
+        P5 = S1 * S5
+        P6 = S4 * B22
+        P7 = A22 * S8
+        
+        T1 = P1 + P2
+        T2 = T1 + P4
+        
+        C11 = P2 + P3
+        C12 = T1 + P5 + P6
+        C21 = T2 - P7
+        C22 = T2 + P5
+
+    Args:
+        a (list): матрица размером N*N.
+        b (list): матрица размером N*N.
+
+    Returns:
+        list: матрица размером N*N произведения матриц a и b.
+    """
+
+    def _Vinograd_Strassen(a: list, b: list):
+        n = len(a)
+        # Базовый случай.
+        if n == 1:
+            return [[a[0][0] * b[0][0]]]
+        
+        # Рекурсивный случай.
+        n2 = n // 2
+
+        A11 = [[a[i][j] for j in range(n2)] for i in range(n2)]
+        A12 = [[a[i][j] for j in range(n2, n)] for i in range(n2)]
+        A21 = [[a[i][j] for j in range(n2)] for i in range(n2, n)]
+        A22 = [[a[i][j] for j in range(n2, n)] for i in range(n2, n)]
+        B11 = [[b[i][j] for j in range(n2)] for i in range(n2)]
+        B12 = [[b[i][j] for j in range(n2, n)] for i in range(n2)]
+        B21 = [[b[i][j] for j in range(n2)] for i in range(n2, n)]
+        B22 = [[b[i][j] for j in range(n2, n)] for i in range(n2, n)]
+
+        S1 = [[A21[i][j] + A22[i][j] for j in range(n2)] for i in range(n2)]
+        S2 = [[S1[i][j] - A11[i][j] for j in range(n2)] for i in range(n2)]
+        S3 = [[A11[i][j] - A21[i][j] for j in range(n2)] for i in range(n2)]
+        S4 = [[A12[i][j] - S2[i][j] for j in range(n2)] for i in range(n2)]
+        S5 = [[B12[i][j] - B11[i][j] for j in range(n2)] for i in range(n2)]
+        S6 = [[B22[i][j] - S5[i][j] for j in range(n2)] for i in range(n2)]
+        S7 = [[B22[i][j] - B12[i][j] for j in range(n2)] for i in range(n2)]
+        S8 = [[S6[i][j] - B21[i][j] for j in range(n2)] for i in range(n2)]
+
+        P1 = _Vinograd_Strassen(S2, S6)
+        P2 = _Vinograd_Strassen(A11, B11)
+        P3 = _Vinograd_Strassen(A12, B21)
+        P4 = _Vinograd_Strassen(S3, S7)
+        P5 = _Vinograd_Strassen(S1, S5)
+        P6 = _Vinograd_Strassen(S4, B22)
+        P7 = _Vinograd_Strassen(A22, S8)
+
+        T1 = [[P1[i][j] + P2[i][j] for j in range(n2)] for i in range(n2)]
+        T2 = [[T1[i][j] + P4[i][j] for j in range(n2)] for i in range(n2)]
+
+        C = [[0 for _ in range(n)] for _ in range(n)]
+        for i in range(n2):
+            for j in range(n2):
+                C[i][j] = P2[i][j] + P3[i][j]
+        for i in range(n2):
+            for j in range(n2):
+                C[i][n2 + j] = T1[i][j] + P5[i][j] + P6[i][j]
+        for i in range(n2):
+            for j in range(n2):
+                C[n2 + i][j] = T2[i][j] - P7[i][j]
+        for i in range(n2):
+            for j in range(n2):
+                C[n2 + i][n2 + j] = T2[i][j] + P5[i][j]
+
+        return C
+
+    from math import trunc, log2
+
+    assert len(a) == len(a[0]) == len(b) == len(b[0]) , "Матрицы a и b должны быть квадратными."
+
+    # Является ли сторона квадратной матриц степенью двойки.
+    n = len(a) 
+    n1 = log2(n)
+
+    # Если да - запускаем алгоритм без предварительных изменений матриц a и b.
+    if n1 % 1 == 0.0:
+        C = _Vinograd_Strassen(a, b)
+        return C
+
+    # Если нет - дополняем входные матрицы a и b до следующей по величине степени двойки.
+    n1 = pow(2, trunc(n1) + 1)   
+    a = [a[i] + [0] * (n1 - n) for i in range(n)] + [[0] * n1 for _ in range(n1 - n)]
+    b = [b[i] + [0] * (n1 - n) for i in range(n)] + [[0] * n1 for _ in range(n1 - n)]
+    C = _Vinograd_Strassen(a, b)
+    # Обрезаем добавленные нули из возвращаемой матрицы, приводя её к исходным размерам.
+    return [[C[i][j] for j in range(n)] for i in range(n)]
 
 
 def gcd(a: int, b: int) -> int:
